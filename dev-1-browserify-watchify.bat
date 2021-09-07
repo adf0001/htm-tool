706907ea4d5bcf4547173cbe8ddab7c658b6b060
fwg <adf0001@163.com>
2021-09-07 13:57:32 +0800

    update test htm template

---
+++ dev-1-browserify-watchify.bat
@@ -0,0 +1,12 @@
+
+chcp 936
+
+set watchifyPath="watchify.cmd"
+
+set module=htm-tool
+
+if not exist ./release md release
+
+%watchifyPath% -o ./release/bundle.js -v ^
+	-r ./%module%.js:%module%
+
